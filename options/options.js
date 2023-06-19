bc2d762a20e9ad2b398ff0d41aca67fcddac6251
Crowdbotics <engineering@crowdbotics.com>
2023-06-19 10:31:38 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "tst-temp-figma-dev-92882",
+  url: "https://tst-temp-figma-dev-92882.botics.co",
+  api: "https://tst-temp-figma-dev-92882.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
